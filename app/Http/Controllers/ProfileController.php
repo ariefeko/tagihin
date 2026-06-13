3b91ef4dfd4798537a486136a68db510acf15c87
Arief Eko <riff.sense@gmail.com>
2026-06-14 03:01:55 +0700

    fix: test railway

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -31,10 +31,6 @@ class ProfileController extends Controller
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
         }
-        
-        dd('test orchesttration');
-        dd('add error lagi');
-        dd('add error lagi');
 
         $request->user()->save();
 
